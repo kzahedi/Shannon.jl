c3769a031dd6b70f557309ebe0776318cab7d759
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2014-06-26 22:52:34 +0200

    Kullback-Leibler divergence
    - Added KL-Divergence on two probability distributions
    
    The function KL(p,q) computes D_KL(p||q) = \sum p(x) log_s (p(x) / q(x))

--- src/Shannon.jl
+++ src/Shannon.jl
@@ -1,5 +1,15 @@
 module Shannon
 
-# package code goes here
+export KL
+
+function KL(p::Vector{Float64}, q::Vector{Float64})
+  r = 0
+  for i=1:length(p)
+    if p[i] != 0 && q[i] != 0
+      r = r + p[i] * log2(p[i]/q[i])
+    end
+  end
+  r
+end
 
 end # module
