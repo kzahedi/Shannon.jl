ae76a6b4ca5623cfcaa4039426950703e40f3a11
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2014-07-17 10:14:55 +0200

    Mutual Information
    - added functions to calculate the mutual information on discrete data
    - added functions to discretise data

--- src/Shannon.jl
+++ src/Shannon.jl
@@ -1,6 +1,10 @@
 module Shannon
 
-export KL
+export KL, MI
+export bin_value, bin_vector, bin_matrix
+export combine_binned_vector, combine_binned_matrix
+export relabel
+export fe1p, fe1ph, fe2p
 
 function KL(p::Vector{Float64}, q::Vector{Float64})
   @assert (length(p) == length(q)) "Size mismatch"
@@ -11,6 +15,86 @@ function KL(p::Vector{Float64}, q::Vector{Float64})
     end
   end
   r
+end
+
+# mutual information
+function MI(data::Vector{Int64})
+  max = maximum(data)
+  px  = fe1p(data[1:end-1])
+  py  = fe1p(data[2:end])
+  pxy = fe2p(hcat(data[1:end-1], data[2:end]))
+
+  r = 0
+  for x=1:length(px)
+    for y=1:length(py)
+      if abs(px[x]) > 0.00000000001 && abs(py[y]) > 0.00000000001 && abs(pxy[x,y]) > 0.00000000001
+        r = r + pxy[x,y] * (log2(pxy[x,y]) - log2(px[x]*py[y]))
+      end
+    end
+  end
+  r
+end
+
+function bin_value(v::Float64, min::Float64, max::Float64, bins::Int64)
+  f = maximum([minimum([1.0, (v-min) / (max - min)]), 0.0])
+  g = maximum([f * bins, 1.0])
+  int64(g)
+end
+
+function bin_vector(vec::Vector{Float64}, min::Float64, max::Float64, bins::Int64)
+  bf(v::Float64) = bin_value(v, min, max, bins)
+  map(bf, vec)
+end
+
+function bin_matrix(m::Matrix{Float64}, min::Float64, max::Float64, bins::Int64)
+  r = zeros(size(m))
+  for i=1:size(m)[1]
+    r[i,:] = bin_vector(squeeze(m[i,:],1), min, max, bins)
+  end
+  convert(Matrix{Int64}, r)
+end
+
+function combine_binned_vector(v::Vector{Int64}, bins::Int64)
+  r = 0
+  for i = 1:length(v)
+    r += v[i] * bins^(i-1)
+  end
+  convert(Int64, r)
+end
+
+function combine_binned_matrix(v::Matrix{Int64})
+  l = size(v)[1]
+  r = zeros(l)
+  m = maximum(v)
+  for i = 1:l
+    r[i] = combine_binned_vector(squeeze(v[i,:],1), m)
+  end
+  convert(Vector{Int64}, r)
+end
+
+relabel(v::Vector{Int64}) = indexin(v, unique(v))
+fe1ph(v::Vector{Int64})   = hist(v)[2] ./ size(v)[1]
+
+function fe2p(v::Matrix{Int64}) # frequency estimation of one dimensional probability 
+  m1 = maximum(v[:,1])
+  m2 = maximum(v[:,2])
+  r  = counts(v[:,1], v[:,2], (1:m1, 1:m2))
+  l  = size(v)[1]
+  r = r ./ l
+  # just to get rid of the numerical inaccuracies and make sure its a probability distribution
+  s = sum(r)
+  r ./ s
+end
+
+# frequency estimation of one dimensional probability 
+function fe1p(v::Vector{Int64})
+  m = maximum(v)
+  l  = length(v)
+  r  = counts(v, 1:m)
+  r = r ./ l
+  # just to get rid of the numerical inaccuracies and make sure its a probability distribution
+  s = sum(r)
+  r ./ s
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,40 @@
 using Shannon
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+v = [0:10] / 10
+r = [1,1:10]
+
+@test r == bin_vector(v, 0.0, 1.0, 10)
+
+r = zeros((10,5))
+i = 1
+for j=1:10
+  for k=1:5
+    r[j,k]=i
+    i = i+1
+  end
+end
+v = r ./ 50
+r = convert(Matrix{Int64}, r)
+
+@test r == bin_matrix(v, 0.0, 1.0, 50)
+
+c = [[1 0 0 0], [1 2 0 0], [1 2 3 0], [1 2 3 4]]
+r = [1, 1 + 2 * 4^1, 1 + 2*4^1 + 3*4^2, 1 + 2*4^1 + 3*4^2 + 4*4^3]
+
+@test 1    == combine_binned_vector(squeeze(c[1,:],1), 10)
+@test 21   == combine_binned_vector(squeeze(c[2,:],1), 10)
+@test 321  == combine_binned_vector(squeeze(c[3,:],1), 10)
+@test 4321 == combine_binned_vector(squeeze(c[4,:],1), 10)
+
+@test r[1] == combine_binned_vector(squeeze(c[1,:],1), 4)
+@test r[2] == combine_binned_vector(squeeze(c[2,:],1), 4)
+@test r[3] == combine_binned_vector(squeeze(c[3,:],1), 4)
+@test r[4] == combine_binned_vector(squeeze(c[4,:],1), 4)
+
+@test r    == combine_binned_matrix(c)
+
+#= println(v) =#
+#= vb = bin_matrix(v, -1.0, 1.0, 10) =#
+#= cb = combine_binned_matrix(vb) =#
+#= println(cb) =#
